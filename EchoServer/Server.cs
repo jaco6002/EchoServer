48d859d18954a9f4fba37ca5d5a73272c6c243e6
jaco6002 <jaco6002@edu.easj.dk>
2020-02-11 12:50:01 +0100

    Add project files.

---
+++ EchoServer/Server.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace EchoServer
+{
+    public class Server
+    {
+        public void Start()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Loopback,7);
+            listener.Start();
+            var socket = listener.AcceptTcpClient();
+            var ns = socket.GetStream();
+            var reader = new StreamReader(ns);
+            var writer = new StreamWriter(ns);
+            string line = reader.ReadLine();
+            writer.WriteLine(line);
+            writer.Flush();
+
+        }
+    }
+}
